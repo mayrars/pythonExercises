df17a2496bd26b57948f43ae92411688837885f6
mayrars <mayra203@gmail.com>
2025-01-14 07:11:27 -0600

    Change students project

--- students/main.py
+++ students/main.py
@@ -1,15 +1,18 @@
 class Student:
 
-    class_year = 2024
+    class_year = 2025
+    num_students=0
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        Student.num_students += 1
         
 student1 = Student("John", 20)
 student2 = Student("Jane", 22)
 student3 = Student("Bob", 21)
 
+print(f"My graduating class of {Student.class_year} has {Student.num_students} students")
 print(student1.name)
-print(student2.age)
-print(student3.class_year)
+print(student2.name)
+print(student3.name)
