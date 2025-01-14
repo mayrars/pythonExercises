3c374a78520c61180a1bf0621e24970d6053c48b
mayrars <mayra203@gmail.com>
2025-01-14 07:12:30 -0600

    Created multiple inheritance project

---
+++ predator/main.py
@@ -0,0 +1,13 @@
+class Prey:
+    def flee(self):
+        print("This animal is fleeing")
+class Predator:
+    def hunt(self):
+        print("This animal is hunting")
+class Rabbit:
+    pass
+class Hawk: 
+    pass
+class Fish:
+    pass
+
