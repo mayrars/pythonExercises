a6997e7a2f7afddb3b02cd6c00131e9363206330
mayrars <mayra203@gmail.com>
2025-01-30 06:25:55 -0600

    select products

--- dbconnect/bd.py
+++ dbconnect/bd.py
@@ -4,14 +4,12 @@ miConexion = sqlite3.connect("PrimeraBD")
 
 miCursor = miConexion.cursor()
 
-variosArticulos = [
-    ("Bicicleta", 3000,"DEPORTES"),
-    ("Portatil", 2500,"COMPUTADORAS"),
-    ("Movil", 500, "TELEFONIA"),
-    ("Tablet", 900,"COMPUTADORAS")
-]
+miCursor.execute("SELECT * FROM PRODUCTOS")
 
-miCursor.executemany("INSERT INTO PRODUCTOS VALUES(?, ?, ?)", variosArticulos)
+variosProductos = miCursor.fetchall()
+
+for i in variosProductos:
+    print("Nombre del producto: ", i[0], "Secciòn:",i[2], " Precio: ", i[1])
 
 miConexion.commit()
 miConexion.close()
