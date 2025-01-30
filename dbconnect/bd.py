b58374142448ecbfbc7e8c5d2206120d02f45299
mayrars <mayra203@gmail.com>
2025-01-30 07:02:02 -0600

    CREATED DB with keys

--- dbconnect/bd.py
+++ dbconnect/bd.py
@@ -4,12 +4,21 @@ miConexion = sqlite3.connect("PrimeraBD")
 
 miCursor = miConexion.cursor()
 
-miCursor.execute("SELECT * FROM PRODUCTOS")
+miCursor.execute('''
+    CREATE TABLE PRODUCTOR (
+        CODIGO INTEGER PRIMARY KEY AUTOINCREMENT,
+        NOMBRE VARCHAR(50),
+        PRECIO INTEGER(10),
+        SECCION VARCHAR(20)
+    )
+''')
 
-variosProductos = miCursor.fetchall()
-
-for i in variosProductos:
-    print("Nombre del producto: ", i[0], "Secciòn:",i[2], " Precio: ", i[1])
-
+productos = [
+    ("Leche", 1000, "Lácteos"),
+    ("Queso", 1500, "Lácteos"),
+    ("Pan", 2000, "Panadería"),
+    ("Galletas", 500, "Panadería"),
+]
+miCursor.executemany("INSERT INTO PRODUCTOR VALUES (NULL,?,?,?)", productos)
 miConexion.commit()
 miConexion.close()
